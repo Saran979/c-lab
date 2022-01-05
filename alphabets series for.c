a4d05701b27797b897ce3397a5ad1dce78a6b1cd
Saran979 <95116383+Saran979@users.noreply.github.com>
2022-01-05 14:26:31 +0530

    Add files via upload

---
+++ alphabets series for.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int k,n;
+	for(n=65;n<=70;n++)
+	{
+		for(k=65;k<=n;k++)
+		{
+			printf("%c\t",k);
+		}
+		printf("\n");
+	}
+}
---
+++ same alphabets series for.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int k,n;
+	for(n=65;n<=70;n++)
+	{
+		for(k=65;k<=n;k++)
+		{
+			printf("%c\t",n);
+		}
+			
+		printf("\n");
+	}
+}
